d967f89cd489ce8063e873d60563179804c49a1c
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-04-19 18:16:07 -0300

    criado componente de icone

---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const WrapperHeader = styled.div `  
+
+display: grid;
+place-items: center;
+
+`
+export const ContainerLogo = styled.div `  
+
+margin: 20px 0;
+`
