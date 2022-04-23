61583e3e02678c49dd3c55a075c09fec96d343c6
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-04-23 11:27:06 -0300

    verificando o campeão do jogo

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GameOption from "../GameOption/GameOption";
 import Icon from "../Icon/Icon";
 import { ContainerGame, ContainerPlayer, WrapperGame } from "./Game.styles";
@@ -6,18 +6,44 @@ import { ContainerGame, ContainerPlayer, WrapperGame } from "./Game.styles";
 export default function Game() {
 
     const [game, setGame] = useState(Array(9).fill(0))
-    const [current, setCurrent] = useState(1)
+    const [current, setCurrent] = useState(-1)
+    const [winner, setWinner] = useState(0)
+
+    const tableWinner = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
 
     const handleClick = (pos) => {
-        if (!game[pos]) {
+        if (!game[pos] && winner === 0) {
             const newGame = [...game]
             newGame[pos] = current
-            setCurrent(current * -1)
             setGame(newGame)
-            console.log(newGame);
         }
 
     }
+
+    const verifyWinner = () => {
+        tableWinner.forEach((line) => {
+            const values = line.map((pos) => game[pos] )
+            const sun = values.reduce((sun, value) => sun + value)
+            if (sun === 3 || sun === -3) {
+                setWinner(sun / 3)
+            }
+        })
+    }
+
+    useEffect(() => {
+
+        setCurrent(current * -1)
+        verifyWinner()
+    },[game])
 
     return (
         <WrapperGame>
