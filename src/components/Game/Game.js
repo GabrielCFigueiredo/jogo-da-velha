a8e05eb63cb50d3683f739d7fa4886348c4dcdfc
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-04-23 10:02:03 -0300

    criado logica do jogo

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,21 +1,51 @@
 import { useState } from "react";
 import GameOption from "../GameOption/GameOption";
-import { WrapperGame } from "./Game.styles";
+import Icon from "../Icon/Icon";
+import { ContainerGame, ContainerPlayer, WrapperGame } from "./Game.styles";
 
 export default function Game() {
 
     const [game, setGame] = useState(Array(9).fill(0))
-    console.log(game);
+    const [current, setCurrent] = useState(1)
+
+    const handleClick = (pos) => {
+        if (!game[pos]) {
+            const newGame = [...game]
+            newGame[pos] = current
+            setCurrent(current * -1)
+            setGame(newGame)
+            console.log(newGame);
+        }
+
+    }
+
     return (
         <WrapperGame>
-            {
-                game.map((value, pos) => <GameOption
-                    key={pos}
-                    status={value}
+            <ContainerGame>
+                {
+                    game.map((value, pos) => <GameOption
+                        key={pos}
+                        status={value}
+                        onClick={() => handleClick(pos)}
                     />
-                )
-            }
-            
+                    )
+                }
+            </ContainerGame>
+            <ContainerPlayer>
+                <h4>
+                    Proximo a jogar:
+                </h4>
+                <div>
+                    {
+                        current === 1 && <Icon iconName={"circle"} />
+                    }
+                    {
+                        current === -1 && <Icon iconName={"x"} />
+                    }
+                </div>
+
+            </ContainerPlayer>
+
         </WrapperGame>
     )
 }
--- src/components/Game/Game.styles.js
+++ src/components/Game/Game.styles.js
@@ -2,9 +2,21 @@ import styled from "styled-components";
 
 export const WrapperGame = styled.div ` 
 
+display: flex;
+justify-content: space-between;
+width: 500px;
+`
+export const ContainerGame = styled.div `  
+
 display: grid;
 place-items: center;
 grid-template-columns: repeat(3, 1fr);
 gap: 16px;
+`
+export const ContainerPlayer = styled.div `  
 
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 24px;
 `
