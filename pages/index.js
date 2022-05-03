ec3660627ca910a4036a719ef735ef33e56cbc66
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-05-03 16:28:26 -0300

    atualizando placar do jogo

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Game from "../src/components/Game/Game";
 import Header from "../src/components/Header/Header";
 import { Container, Wrapper } from "./index.styles";
@@ -15,3 +16,23 @@ export default function Home() {
 
   )
 }
+
+
+
+
+Wrapper = styled.div` 
+
+display: grid;
+place-items: center;
+max-width: 100vw;
+max-height: 100vh;
+`
+
+Container = styled.div`  
+
+display: grid;
+place-items: center;
+gap: 16px;
+height: 60%;
+width: 100%;
+` 
