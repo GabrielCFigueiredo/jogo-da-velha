1f96b67ae1fea50abfcff0d8ecdb71d0a6d420c8
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-05-03 16:19:58 -0300

    placar do jogo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Game from "../src/components/Game/Game";
 import Header from "../src/components/Header/Header";
-import { Container, Wrapper } from "./page.styles/index.styles";
+import { Container, Wrapper } from "./index.styles";
+
 
 
 export default function Home() {
--- src/components/GameOption/GameOption.styles.js
+++ src/components/GameOption/GameOption.styles.js
@@ -2,11 +2,10 @@ import styled from "styled-components";
 
 export const WrapperGameOption = styled.div`  
 
-border: ${props => (props.winner ? "2px solid #00eeff" : "2px solid #ffff")} ;
-border: ${props => (props.draw ? "2px solid #EBFC00" : "2px solid #ffff")} ;
+background-color: ${props => (props.winner ? "#00eeff" : "#000")};
+border: ${props => (props.draw ? "2px solid #EBFC00" : "2px solid #ffff")};
 width: 50px;
 height: 50px;
 display: grid;
 place-items: center;
-
 `
